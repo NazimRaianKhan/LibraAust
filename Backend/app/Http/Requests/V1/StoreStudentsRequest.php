bfe2b3b6d0ad17520cbebc9e585563b20665e9f3
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-09-05 20:11:20 +0600

    Updated Backend Requirements #31

--- Backend/app/Http/Requests/V1/StoreStudentsRequest.php
+++ Backend/app/Http/Requests/V1/StoreStudentsRequest.php
@@ -27,13 +27,13 @@ class StoreStudentsRequest extends FormRequest
             'name' => ['required'],
             'department' => ['required', Rule::in(['CSE', 'EEE', 'BBA', 'ME', 'TE', 'CE', 'IPE', 'ARCH'])],
             'semester' => ['required', Rule::in(['1.1', '1.2', '2.1', '2.2', '3.1', '3.2', '4.1', '4.2'])],
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email', 'regex:/^[\w\.-]+@aust\.edu$/i',],
             'phone' => ['nullable'],
             'studentship' => ['required'],
             // Remember to change this later in camelcase
             'student_id' => ['required'],
             'borrowed_id' => ['nullable'],
-            'password' => ['required', 'min:8'], // Assuming borrowed_id refers to a book's id
+            'password' => ['required', 'min:8', 'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/'], // Assuming borrowed_id refers to a book's id
         ];
     }
 
